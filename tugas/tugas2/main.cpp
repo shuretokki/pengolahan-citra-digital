97c5fbf02297bce80cde5f5de7b7c960c630f43a
Tri R. Utomo <tri.r.utomo@gmail.com>
2026-02-16 23:28:38 +0700

    add raw sizes

--- tugas/tugas2/main.cpp
+++ tugas/tugas2/main.cpp
@@ -9,7 +9,7 @@ using namespace spdlog;
 
 int main() {
   const std::string assets = ROOT "/common/assets/";
-  auto img8 = imread(assets + "tugeas2.png", IMREAD_UNCHANGED);
+  auto img8 = imread(assets + "tugas2.png", IMREAD_UNCHANGED);
   if (img8.empty()) {
     error("can't open or read file");
     return -1;
@@ -18,19 +18,16 @@ int main() {
   /*//
     (2); 2.4.1 sampling
       M (rows) x N (cols)
+      M x N = total pixels;
+      M x N x b = bit raw sizes;
+      M x N x B = byte raw sizes;
+      Where raw means it is uncompressed;
   *///
   print("{} x {}\n", img8.rows, img8.cols);
   print("{} total pixels\n", img8.total());
-
-  /*//
-    (3); 2.4.1 quantization
-      L = 2 ^ bits(k)
-
-      1  bit = 2 levels
-      8  bit = 256 levels
-      16 bit = 65536 levels
-  *///
-  print("{} gray levels\n", 1 << img8.elemSize1() * 8);
+  print("{}KB or {}Kb uncompressed sizes \n",
+        (img8.total() * img8.elemSize()) / 1000,
+        (img8.total() * img8.elemSize() * 8) / 1000);
 
   /*//
     elemSize1() return size of one color value;
@@ -41,6 +38,16 @@ int main() {
   print("{} byte or ", img8.elemSize1());
   print("{} bits per channel\n", img8.elemSize1() * 8);
   print("{} bytes each pixel\n", img8.elemSize());
+
+  /*//
+    (3); 2.4.1 quantization
+      L = 2 ^ bits(k)
+
+      1  bit = 2 levels
+      8  bit = 256 levels
+      16 bit = 65536 levels
+  */ //
+  print("{} gray levels\n", 1 << img8.elemSize1() * 8);
 
   /*//
     channels();
